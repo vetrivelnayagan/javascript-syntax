f45a17b8a0c7a36c275927e7b1cbc687576457c5
vetrivelnayagan <89455598+vetrivelnayagan@users.noreply.github.com>
2021-10-06 15:51:12 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+var firstname="Vetrivel"
+var secondname="Nayagan"
+var fullmame=firstname.concat(secondname)
+console.log(fullmame)
+var len=fullmame.length
+console.log(len)
+var name="jack john"
+var replacename=name.replace("john","sam")
+console.log(replacename)
+var uppercase=fullmame.toUpperCase()
+console.log(uppercase)
+var squareroot=Math.sqrt(25)
+console.log (squareroot)
+
+var max=Math.max(10,15)
+console.log(max)
+var min=Math.min(10,15)
+console.log(min)
+var power=Math.pow(4,4)
+console.log(power)
+var a=10
+var b=20
+console.log(a+b)
+console.log(a-b)
+console.log(a*b)
+console.log(a/b)
